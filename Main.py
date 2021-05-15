ce3aa2c839821c76856fb5d1561098c4c74f1ad0
Jakub Haczyński <01158873@pw.edu.pl>
2021-05-15 06:38:37 +0200

    a teraz nawet testowe pliki można robić w formacie [co jest i co ma byc]

--- Main.py
+++ Main.py
@@ -11,6 +11,17 @@ gridSize = 6  # musi być wielokrotnością 2
 drawGridLines = True
 updateTime = 0.1
 board = [];
+file =open("test")
+tablicatestowa=[]
+for i in file:
+    t=i.split(" ")
+    for j in t:
+        x=j.split(',')
+        for w in x:
+            w=int(w)
+        tablicatestowa.append([int(x[0]),int(x[1]),int(x[2]),int(x[3])])
+
+
 
 
 def initBoard():
@@ -73,7 +84,11 @@ def neighbourCount(x, y):
 
     neighbours = [UL, UR, DL, DR]
     return neighbours
-def transform(UL,UR,DL,DR,x,y,boards):
+def transform(tab,x,y,boards):
+                UL=int(tab[0])
+                UR=int(tab[1])
+                DL=int(tab[2])
+                DR=int(tab[3])
 
                 if x == gridSize - 1 and y < gridSize - 1:
                     boards[y][gridSize - 1]=UL
@@ -104,17 +119,25 @@ def processBoard():
         global board
         for y in range(0,int(len(board)),2):
             for x in range(0,int(len(board[y])),2):
-                nc = neighbourCount(x,y)
-                # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
-                if nc == [1, 0, 0, 0]:
-                    transform(0,0,0,1,x,y,board)
+                for i in range(0, len(tablicatestowa), 2):
+                    nc = neighbourCount(x,y)
+                    # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
+
+                    if nc == tablicatestowa[i]:
+                            test = tablicatestowa[i+1]
+                            print(test)
+                            transform(test, x, y, board)
 def processBoard2():
         for y in range(1,int(len(board)),2):
             for x in range(1,int(len(board[y])),2):
-                nc = neighbourCount(x,y)
-                # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
-                if nc == [1, 0, 0, 0]:
-                   transform(0, 0, 0, 1, x,y,board)
+                for i in range(0, len(tablicatestowa), 2):
+                    nc = neighbourCount(x, y)
+                    # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
+
+                    if nc == tablicatestowa[i]:
+                        test = tablicatestowa[i + 1]
+                        print(test)
+                        transform(test, x, y, board)
 
 
 
@@ -130,8 +153,8 @@ def drawCells():
 pattern = [
     [1, 0, 0, 0, 0, 0, ],
     [0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, ],
-    [0, 0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, ],
+    [1, 0, 0, 0, 0, 0, ],
     [0, 0, 0, 0, 0, 0, ],
     [0, 0, 0, 0, 0, 0, ],
 ]
