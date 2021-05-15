abcc8199716903792645dab6ab6d51c4133e9b02
Piotr Baprawski <01158783@pw.edu.pl>
2021-05-15 14:43:04 +0200

    Poprawki v4

--- Main.py
+++ Main.py
@@ -3,19 +3,29 @@ from datetime import datetime
 import random
 import numpy as np
 from pygame.locals import *
-
+#Definicja kolorów RGB
 white = (255, 255, 255)
 black = (0, 0, 0)
-(width, height) = (800, 800)  # Dimension of the window
-#screen = pygame.display.set_mode((width, height))  # Making of the screen
-pygame.display.set_caption("Cellular automaton")
+(width, height) = (800, 800)  # Rozmiar okna
+#Nazwa okna
+pygame.display.set_caption("Automat Komórkowy")
+#Flaga podwójnego buforu (zwiększa wydajność)
 flags = DOUBLEBUF
+#Rozdzielczość na podstawie rozmiarów okna
 resolution=[width,height]
+#Zdefiniowanie okna
 screen = pygame.display.set_mode(resolution, flags, 16)
+#Rozmiar siatki
 gridSize = 100  # musi być wielokrotnością 2
+#Opcja renderowania linii siatki
 drawGridLines = False
+#Minimalny interwał pomiedzy kolejnymi klatkami
 updateTime = 0.01
-board = [];
+#Inicjalizacja tablicy
+board = []
+#odczytanie pliku z zdefiniowanymi zasadami gry, w formacie X,X,X,X Y,Y,Y,Y
+# kolejne X to stan sprawdzany, a Y- stan generowany w kolejności Lewy górny róg,
+#Prawy górny róg, Lewy dolny róg, prawy dolny róg
 file =open("test")
 tablicatestowa=[]
 for i in file:
@@ -28,7 +38,7 @@ for i in file:
 
 
 
-
+#Funkcja inicjalizująca tablicę o rozmiarze gridSize x gridSize
 def initBoard():
     for y in range(gridSize):
         row = []
@@ -36,7 +46,7 @@ def initBoard():
             row.append(0)
         board.append(row)
 
-
+#Funkcja rysująca linie siatki
 def drawGrid():
     for y in range(1, gridSize):
         pygame.draw.line(screen, black, (0, int((height / gridSize) * y)), (width, int((height / gridSize) * y)), 1)
@@ -45,8 +55,8 @@ def drawGrid():
         pygame.draw.line(screen, black, (int((width / gridSize) * x), 0), (int((width / gridSize) * x), height), 1)
 
 
-
-def fillCell2(x, y):
+#Funkcja wypełniająca białe piksele
+def fillCellWhite(x, y):
     if x < 0 or x > gridSize or y < 0 or y > gridSize:
         raise Exception("Invalid coords: " + str(x) + ":" + str(y));
 
@@ -54,16 +64,15 @@ def fillCell2(x, y):
         x * int(width / gridSize), int(y * (height / gridSize)), int(width / gridSize), int(height / gridSize)))
 
 
-
-
-def fillCell(x, y):
+#Funkcja wypełniająca czarne piksele
+def fillCellBlack(x, y):
     if x < 0 or x > gridSize or y < 0 or y > gridSize:
         raise Exception("Invalid coords: " + str(x) + ":" + str(y));
 
     pygame.draw.rect(screen, black, (
         x * int(width / gridSize), int(y * (height / gridSize)), int(width / gridSize), int(height / gridSize)))
 
-
+#Funkcja umieszczająca piksele w odpowiedniej części tablicy zależnej od offsetu
 def placePattern(pattern, offset_x, offset_y):
     global board
     for y in range(len(pattern)):
@@ -71,7 +80,7 @@ def placePattern(pattern, offset_x, offset_y):
             if offset_y + y < len(board) and offset_x + x < len(board[offset_y + y]):
                 board[offset_y + y][offset_x + x] = pattern[y][x]
 
-
+#Funkcja sprawdzająca sąsiednie komórki do zadanej
 def neighbourCount(x, y):
     UR = 0
     DR = 0
@@ -100,6 +109,7 @@ def neighbourCount(x, y):
 
     neighbours = [UL, UR, DL, DR]
     return neighbours
+#Funkcja przekształcająca dane komórki na podstawie wartości tablicy tab
 def transform(tab,x,y,boards):
                 UL=int(tab[0])
                 UR=int(tab[1])
@@ -130,26 +140,27 @@ def transform(tab,x,y,boards):
                 boards=board
                 return board
 
-
+#Funkcja obliczająca zmiany dla siatki 2x2 bez przesunięcia
 def processBoard():
         global board
         for y in range(0,int(len(board)),2):
             for x in range(0,int(len(board[y])),2):
                 for i in range(0, len(tablicatestowa), 2):
                     nc = neighbourCount(x,y)
-                    # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
+                    #definicja zasad według których działa funkcja transform
 
                     if nc == tablicatestowa[i]:
                             test = tablicatestowa[i+1]
                             transform(test, x, y, board)
         return board
+#Funkcja obliczająca zmiany dla siatki 2x2 z przesunięciem o 1 piksel w każdym wymiarze
 def processBoard2():
         global board
         for y in range(1,int(len(board)),2):
             for x in range(1,int(len(board[y])),2):
                 for i in range(0, len(tablicatestowa), 2):
                     nc = neighbourCount(x, y)
-                    # tutaj zdefiniujemy zasady a potem x i y +1 i mamy drugą siatke K
+                    #definicja zasad według których działa funkcja transform
 
                     if nc == tablicatestowa[i]:
                         test = tablicatestowa[i + 1]
@@ -158,16 +169,16 @@ def processBoard2():
 
 
 
-
+#Funkcja rysująca komórki na podstawie wartości x,y
 def drawCells():
     for y in range(len(board)):
         for x in range(len(board[y])):
             if board[y][x] == 1:
-                fillCell(x, y)
+                fillCellBlack(x, y)
             if board[y][x] == 0:
-                fillCell2(x, y)
+                fillCellWhite(x, y)
 
-
+#Przykładowy wzór do wykorzystania w ramach testów
 pattern = [
 [1,1,1,1,1,1,1,1 ],
 [1,1,1,1,1,1,1,1 ],
@@ -178,36 +189,28 @@ pattern = [
 [1,1,1,1,1,1,1,1 ],
 [1,1,1,1,1,1,1,1 ]
 ]
+#Funkcja generująca losowe wzory
 def drawrandompatern(size):
     tab= np.random.randint(2,size=(size, size))
     return tab
 
 
-
-# pattern = [
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-# ]
-
+#inicjalizacja tablicy
 initBoard()
-placePattern(drawrandompatern(20), 0, 0)
+#Umieszczenie wzoru w tablicy
+placePattern(drawrandompatern(14), 0, 0)
 #placePattern(pattern,0,0)
 
-running = True
+#Pobranie aktualnego czasu
 prevTime = datetime.now()
-
+#Pętla zapewniająca ciągłość pracy programu
 while 1:
+    #pobranie drugiej próbki czasu
     time = datetime.now()
+    #Funkcja wypełniająca ekran
     screen.fill(white)
-
+    #Na podstawie zmiany w pobranych próbkach czasu wykonywana jest procedura
+    #odświeżenia tablicy z naniesieniem zmian
     if (time - prevTime).total_seconds() > updateTime:
         processBoard()
         drawCells()
@@ -218,13 +221,14 @@ while 1:
         prevTime = time
         drawGridLines=not drawGridLines
 
-
-    print("test")
+    #Opcja narysowania linii siatki w zależnosci od przyjętego wcześniej parametru
     if drawGridLines:
         drawGrid()
-
+    #Narysowanie tablicy
     drawCells()
+    #Wyświetlenie zmian
     pygame.display.flip()
+    #Możliwość zamknięcia programu
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
